3d8b6062282b604abb2b53d0c19db7c46518c305
Jhoestar <202202883@est.umss.edu>
2024-08-28 16:10:51 -0400

    js

--- JavaScript/Peliculas/script.js
+++ JavaScript/Peliculas/script.js
@@ -1,20 +1,59 @@
 
 
 
-const buscar = document.getElementById('search');
-buscar.addEventListener('click',searchMovies);
-let apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyN2YwM2FjZGM4MTNiMjU1ODNiNTBhMGE1ZWUxZjE2YSIsIm5iZiI6MTcyNDc5NTMzMy45NTgxOTUsInN1YiI6IjY2Y2U0N2RiODM3Nzg3MGUxMGRlMDJmMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ro1WZeE3dojfU4dE9BH-RLnDMKvRzgG97-JU4rc5ReI';
+document.getElementById('searchButton').addEventListener('click',searchMovies);
+
+const apiKey = '27f03acdc813b25583b50a0a5ee1f16a';
+const url = 'https://api.themoviedb.org/3/search/movie';
+const urlImg = 'https://image.tmdb.org/t/p/w500';
 
 
-const searchMovies = ()=>{
-    const searchInput = document.getElementById('searchInput');
-    const results = document.getElementById('results');
+const resultsContainer = document.getElementById('results');
 
-    if(!pelicula){
-        results.appendChild('h2')
-        results.textContent = 'Pelicula no encontrada'
-    }else{
-        for
 
+function searchMovies(){
+    resultsContainer.innerHTML = 'Cargando...';
+    let searchInput = document.getElementById('searchInput').value;
+    
+    fetch(`${url}?query=${searchInput}&api_key=${apiKey}`)
+    .then(response => response.json())
+    .then(response => displayMovies(response.results))
+}
+
+
+function displayMovies(movies){
+
+    resultsContainer.innerHTML = '';
+    if(movies.length === 0){
+        resultsContainer.innerHTML = '<p>no se encontraron peliculas para tu busqueda</p>';
+        return
     }
+
+   
+    movies.forEach(movie => {
+        let movieDiv = document.createElement('div');
+        movieDiv.classList.add('movie');
+
+
+        let title = document.createElement('h2');
+        title.textContent = movie.title
+
+        let releaseDate = document.createElement('p');
+        releaseDate.textContent = 'la fecha de lanzamiento fue: ' + movie.release_date
+        
+        let overview = document.createElement('p');
+        overview.textContent = movie.overview
+
+        let poster = document.createElement('img');
+        poster.src = `${urlImg}${movie.poster_path}`;
+
+        movieDiv.appendChild(poster);
+        movieDiv.appendChild(title);
+        movieDiv.appendChild(releaseDate);
+        movieDiv.appendChild(overview);
+
+        resultsContainer.appendChild(movieDiv)
+        });
+
+
 }
